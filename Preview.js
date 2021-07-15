20508160c334fe7927dca951aa284779ef374ab7
Victorcreed07 <79251777+Victorcreed07@users.noreply.github.com>
2021-07-15 13:59:18 +0530

    Create Preview.js

---
+++ Preview.js
@@ -0,0 +1,15 @@
+import React from "react"
+class Preview extends React.Component{
+    
+render(){
+  return (
+    <div>
+      {this.props.markdown}
+      </div>
+      
+    
+  );
+}
+}
+
+export default Preview
